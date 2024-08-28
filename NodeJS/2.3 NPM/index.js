821ac537c4071519416fbcecd4e786a78e8d39be
Dimitar Dimitrov <dimitrov.georgiev91@gmail.com>
2024-08-28 10:41:51 +0300

    Node - npm packages

---
+++ NodeJS/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// let generateName = require('sillyname');
+
+// ES5
+// import generateName from "sillyname";
+import superheroes from "superheroes";
+
+// let sillyName = generateName();
+const superHero = superheroes.random();
+
+// console.log(`My name is ${sillyName}.`);
+console.log(`I am ${superHero}.`);
