02ce62aabf1e2902d879423a0bd9026a005b6a98
Dimitar Dimitrov <dimitrov.georgiev91@gmail.com>
2024-10-11 15:58:08 +0300

    Link to localhost added

--- Express/Middlewares/index4.js
+++ Express/Middlewares/index4.js
@@ -27,5 +27,5 @@ app.post("/submit", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+    console.log(`Listening on port http://localhost:${port}`);
 });
